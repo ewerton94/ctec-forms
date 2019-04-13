e7d9064402ccd3babead9f72a52697c3815d36e4
Ewerton Oliveira <ewerton.amorim@live.com>
2019-04-13 14:18:43 -0300

    Fix utf-8 problems

--- data/models.py
+++ data/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 import random
 import string
 from django.db import models
+import sys
 
 def rand_slug(n):
     return ''.join(random.choice(string.ascii_letters) for _ in range(n))
@@ -19,8 +20,15 @@ class Course(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=4)
-    def __str__(self):
+    def __unicode__(self):
         return self.name
+
+    if sys.version_info[0] >= 3: # Python 3
+        def __str__(self):
+            return self.__unicode__()
+    else:  # Python 2
+        def __str__(self):
+            return self.__unicode__().encode('utf8')
 
 class Subject(models.Model):
     id = models.AutoField(primary_key=True)
@@ -30,8 +38,16 @@ class Subject(models.Model):
     eletiva = models.BooleanField()
     grade = models.IntegerField()
 
-    def __str__(self):
+    def __unicode__(self):
         return self.name
+
+
+    if sys.version_info[0] >= 3: # Python 3
+        def __str__(self):
+            return self.__unicode__()
+    else:  # Python 2
+        def __str__(self):
+            return self.__unicode__().encode('utf8')
 
 class Option(models.Model):
     class Meta:
@@ -40,8 +56,17 @@ class Option(models.Model):
         verbose_name_plural = 'Opções'
     name = models.CharField(max_length=250)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.name
+
+
+
+    if sys.version_info[0] >= 3: # Python 3
+        def __str__(self):
+            return self.__unicode__()
+    else:  # Python 2
+        def __str__(self):
+            return self.__unicode__().encode('utf8')
 
 class Question(models.Model):
     class Meta:
@@ -62,14 +87,35 @@ class Question(models.Model):
                     break
         super(Question, self).save(**kwargs)
 
+    def __unicode__(self):
+        return self.name
+
+    if sys.version_info[0] >= 3: # Python 3
         def __str__(self):
-            return self.name
+            return self.__unicode__()
+    else:  # Python 2
+        def __str__(self):
+            return self.__unicode__().encode('utf8')
+
+
 
 class Group(models.Model):
     class Meta:
         pass
     name = models.CharField(max_length=1000)
     questions = models.ManyToManyField(Question, related_name='groups', blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+    if sys.version_info[0] >= 3: # Python 3
+        def __str__(self):
+            return self.__unicode__()
+    else:  # Python 2
+        def __str__(self):
+            return self.__unicode__().encode('utf8')
+
 
 class Form(models.Model):
     class Meta:
@@ -83,8 +129,6 @@ class Form(models.Model):
     situation = models.BooleanField()
     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
 
-    def __str__(self):
-        return self.name
 
     def is_editable(self):
         results = Result.objects.filter(form=self.id)
@@ -100,8 +144,17 @@ class Form(models.Model):
                     break
         super(Form, self).save(**kwargs)
 
+
+    def __unicode__(self):
+        return self.name
+
+
+    if sys.version_info[0] >= 3: # Python 3
         def __str__(self):
-            return self.name
+            return self.__unicode__()
+    else:  # Python 2
+        def __str__(self):
+            return self.__unicode__().encode('utf8')
 
 
 class Result(models.Model):
@@ -115,5 +168,13 @@ class Result(models.Model):
     answer = models.TextField()
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True, blank=True)
 
-    def __str__(self):
+    def __unicode__(self):
         return '%s para a pergunta "%s" do formulário "%s".'%(self.answer, str(self.question), str(self.form))
+
+
+    if sys.version_info[0] >= 3: # Python 3
+        def __str__(self):
+            return self.__unicode__()
+    else:  # Python 2
+        def __str__(self):
+            return self.__unicode__().encode('utf8')
