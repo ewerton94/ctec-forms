ec4c6ad0f8c0ec71a1c59d5c47d22b12c7a2c66f
Ewerton Oliveira <ewerton.amorim@live.com>
2019-04-22 23:42:35 -0300

    Print function Py2

--- data/views.py
+++ data/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from __future__ import print_function
 from django.shortcuts import render, HttpResponseRedirect
 from django.http import QueryDict
 from django.db import transaction
