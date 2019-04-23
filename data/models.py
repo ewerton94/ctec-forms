ddf4ee535a16ff44fdcbc13db115f5316bf26594
Ewerton Oliveira <ewerton.amorim@live.com>
2019-04-23 00:13:44 -0300

    Corrigido utf8

--- data/models.py
+++ data/models.py
@@ -168,13 +168,12 @@ class Result(models.Model):
     answer = models.TextField()
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True, blank=True)
 
-    def __unicode__(self):
-        return '%s para a pergunta "%s" do formulário "%s".'%(self.answer, str(self.question), str(self.form))
+
 
 
     if sys.version_info[0] >= 3: # Python 3
         def __str__(self):
-            return self.__unicode__()
+            return '%s para a pergunta "%s" do formulário "%s".'%(self.answer, str(self.question), str(self.form))
     else:  # Python 2
         def __str__(self):
-            return self.__unicode__().encode('utf8')
+            return '%s para a pergunta "%s" do formulário "%s".'%(self.answer.encode('utf8'), str(self.question.name.encode('utf8')), self.form.name.encode('utf8')))
