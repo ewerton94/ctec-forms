c5231d1f2a51481ceabf807ac6634c633faf5a37
Ewerton Oliveira <ewerton.amorim@live.com>
2019-04-22 23:37:41 -0300

    Associação do formulário ao banco de dados

--- data/views.py
+++ data/views.py
@@ -2,8 +2,15 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponseRedirect
 from django.http import QueryDict
-from .models import Form, Course, Subject
+from django.db import transaction
+from .models import Form, Course, Subject, Option, Result
 
+def get_order_response(form):
+    r = Result.objects.filter(form=form)
+    if r:
+        return max(r, key=lambda x:x.order).order + 1
+    else:
+        return 1
 
 class Object(object):
     pass
@@ -26,11 +33,54 @@ def subjects(request, form_public_id, course_code=None):
     course = Course.objects.get(code=course_code)
     if 'subjects' in query:
         ids = list(map(int, query.get('subjects').split(',')))
-        subjects = Subject.objects.filter(pk__in=ids)
+        subjects = Subject.objects.select_related().filter(pk__in=ids)
         if request.method == 'POST':
-            print(request.POST)
-            a = dict(request.POST)
-            print(a)
+            with transaction.atomic():
+                data = dict(request.POST)
+                order = get_order_response(form)
+                for subject in subjects:
+                    for group in form.groups.all():
+                        for question in group.questions.all():
+
+                            if question.type == 'text':
+                                option = None
+                                answer = data.get(question.public_id + '_by_' + str(subject.id))[0]
+                            if question.type in ['range', 'number']:
+                                option = None
+                                answer = data.get(question.public_id + '_by_' + str(subject.id))[0]
+
+                            elif question.type in ['radio', 'select']:
+                                answer = data.get(question.public_id + '_by_' + str(subject.id))[0]
+                                if answer is None:
+                                    return render(request, 'subjects.html',
+                                        {
+                                            'subjects': subjects,
+                                            'form': form,
+                                            'course': course,
+                                            'error': 'Você tentou enviar uma resposta faltando informações. Por favor, tente novamente!'
+                                    })
+                                option = Option.objects.get(id=answer)
+                            Result.objects.create(
+                                order=order,
+                                question=question,
+                                form=form,
+                                answer=answer,
+                                subject=subject
+                            )
+                return render(request, 'subjects.html',
+                    {
+
+
+                        'success': 'Obrigado por contribuir com este formulário!'
+                })
+
+
+
+
+
+                keys = [e for e in a.keys() if '_by_' in e]
+                keys = sorted(keys, key=lambda x: x.split('_by_')[1])
+                print(*keys, sep='\n')
         return render(request, 'subjects.html',
             {
                 'subjects': subjects,
